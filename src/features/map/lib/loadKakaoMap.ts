1865be18eacf6b10a2a646ccd9d8f15f0796cd52
ghkd1418 <1004wipi@gmail.com>
2025-05-28 12:45:37 +0900

    feat: 카카오 지도 가져오기 [TOKTOT-12]

---
+++ src/features/map/lib/loadKakaoMap.ts
@@ -0,0 +1,12 @@
+export async function loadKakaoMap(): Promise<void> {
+	if (window.kakao) return;
+
+	return new Promise((resolve) => {
+		const script = document.createElement('script');
+		script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
+		script.onload = () => {
+			window.kakao.maps.load(resolve);
+		};
+		document.head.appendChild(script);
+	});
+}
