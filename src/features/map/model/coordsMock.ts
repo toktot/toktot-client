6a34ecca7cd262cb38ae006f880b8fd3fb90e318
ghkd1418 <1004wipi@gmail.com>
2025-05-28 12:51:32 +0900

    feat: 마커 목데이터 추가 및 지도 컴포넌트 작성

---
+++ src/features/map/model/coordsMock.ts
@@ -0,0 +1,52 @@
+export const SEOUL_TOUR_SPOTS = [
+	{
+		id: 'gyeongbokgung',
+		name: '경복궁',
+		position: { lat: 37.579617, lng: 126.977041 },
+	},
+	{
+		id: 'bukchon',
+		name: '북촌한옥마을',
+		position: { lat: 37.582604, lng: 126.983998 },
+	},
+	{
+		id: 'insadong',
+		name: '인사동',
+		position: { lat: 37.574017, lng: 126.984979 },
+	},
+	{
+		id: 'namsan',
+		name: '남산서울타워',
+		position: { lat: 37.551169, lng: 126.988227 },
+	},
+	{
+		id: 'dongdaemun',
+		name: '동대문디자인플라자',
+		position: { lat: 37.566479, lng: 127.009369 },
+	},
+	{
+		id: 'myeongdong',
+		name: '명동거리',
+		position: { lat: 37.563757, lng: 126.985191 },
+	},
+	{
+		id: 'cheonggyecheon',
+		name: '청계천',
+		position: { lat: 37.570377, lng: 126.991125 },
+	},
+	{
+		id: 'hanriver',
+		name: '한강공원 여의도',
+		position: { lat: 37.527061, lng: 126.932617 },
+	},
+	{
+		id: 'lotteworld',
+		name: '롯데월드',
+		position: { lat: 37.511023, lng: 127.098149 },
+	},
+	{
+		id: 'coex',
+		name: '코엑스몰',
+		position: { lat: 37.511225, lng: 127.059708 },
+	},
+];
---
+++ src/features/map/ui/Map.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useCurrentLocation } from '@/shared/location/lib/useCurrentLocation';
+
+import { SEOUL_TOUR_SPOTS } from '../model/coordsMock';
+import { useMap } from '../model/useMap';
+import { CurrentLocationMarker } from './CurrentLocationMarker';
+import { Marker } from './Marker';
+
+const Map = () => {
+	const { location } = useCurrentLocation();
+	const map = useMap('map', {
+		center: {
+			lat: location?.coords.latitude,
+			lng: location?.coords.longitude,
+		},
+	});
+
+	return (
+		<div style={{ width: '500px', height: '500px' }}>
+			<div id="map" style={{ width: '100%', height: '100%' }} />
+			{map && (
+				<>
+					{/* 내 위치 마커 */}
+					<CurrentLocationMarker map={map} />
+
+					{/* 음식점 마커 */}
+					{SEOUL_TOUR_SPOTS.map((spot) => (
+						<Marker
+							key={spot.id}
+							map={map}
+							position={spot.position}
+							onClick={() => console.log(`클릭한 장소: ${spot.name}`)}
+						/>
+					))}
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Map;
