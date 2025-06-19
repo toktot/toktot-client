fd9c78a3599b182b91bc060ecbbfcf407b4ea38c
ghkd1418 <1004wipi@gmail.com>
2025-06-19 12:59:04 +0900

    chore: drawing 카카오맵 라이브러리 추가 [TOKTOT-12]

--- src/features/map/lib/loadKakaoMap.ts
+++ src/features/map/lib/loadKakaoMap.ts
@@ -3,7 +3,7 @@ export async function loadKakaoMap(): Promise<void> {
 
 	return new Promise((resolve) => {
 		const script = document.createElement('script');
-		script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false&libraries=clusterer`;
+		script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false&libraries=clusterer,drawing`;
 		script.onload = () => {
 			window.kakao.maps.load(resolve);
 		};
