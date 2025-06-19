68117c1c2f3bc6b0490e2ca2f458ce073f668f47
ghkd1418 <1004wipi@gmail.com>
2025-06-19 13:08:35 +0900

    chore: 기본 중앙 좌표 제주도청으로 변경 [TOKTOT-12]

--- src/features/map/model/mapConfig.ts
+++ src/features/map/model/mapConfig.ts
@@ -2,5 +2,12 @@ import type { Coordinates } from '@/shared/location/model/types';
 
 import type { MapZoomLevel } from './types';
 
-export const DEFAULT_CENTER: Coordinates = { lat: 37.56671, lng: 126.97851 };
-export const DEFAULT_LEVEL: MapZoomLevel = 4;
+/**
+ * @description 제주 도청 좌표
+ */
+export const DEFAULT_CENTER: Coordinates = {
+	lat: 33.488898,
+	lng: 126.49809,
+};
+
+export const DEFAULT_LEVEL: MapZoomLevel = 9;
