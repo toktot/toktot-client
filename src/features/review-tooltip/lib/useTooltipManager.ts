11dc9de741b41dd8716375e63167e1cf65e8b96d
ghkd1418 <1004wipi@gmail.com>
2025-06-22 18:13:53 +0900

    feat: 툴팁 5개 제한 및 초과 시 알림 [TOKTOT-93]

--- src/features/review-tooltip/lib/useTooltipManager.ts
+++ src/features/review-tooltip/lib/useTooltipManager.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { MAX_TOOLTIP_COUNT } from '@/entities/review/model/constants';
 import { Tooltip } from '@/entities/review/model/tooltip';
 import { nanoid } from 'nanoid';
 
@@ -7,6 +8,10 @@ export function useTooltipManager() {
 	const [tooltips, setTooltips] = useState<Tooltip[]>([]);
 
 	const addTooltip = (tooltip: Omit<Tooltip, 'id'>) => {
+		if (tooltips.length >= MAX_TOOLTIP_COUNT) {
+			alert('최대 5개까지만 등록할 수 있습니다.');
+			return;
+		}
 		const newTooltip = { ...tooltip, id: nanoid() } as Tooltip;
 
 		setTooltips((prev) => [...prev, newTooltip]);
