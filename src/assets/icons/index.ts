bf7bf88dc1cd5812ca6a3af6f2f9966c62291f3f
hyewon <won10201@kookmin.ac.kr>
2025-07-02 21:35:33 +0900

    fix: 검색 아이콘 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -34,3 +34,4 @@ export { default as Trash } from './trash.svg';
 export { default as Variant23 } from './Variant23.svg';
 export { default as WarningMark } from './warningmark.svg';
 export { default as WesternFood } from './western-food.svg';
+export { default as Search } from './search.svg';
