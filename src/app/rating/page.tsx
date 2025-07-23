2a86d7e26a4bf6db3b72584944fc3f9fc86b41e7
ghkd1418 <1004wipi@gmail.com>
2025-07-23 19:06:47 +0900

    chore: eslint ignore 파일 설정에 추가

--- src/app/rating/page.tsx
+++ src/app/rating/page.tsx
@@ -3,11 +3,11 @@
 import ReviewDetailSheet from '@/features/review/components/ReviewDetail';
 
 export default function ReviewTestPage() {
-  return (
-    <main className="min-h-screen bg-grey-5 flex items-center justify-center px-4 py-12">
-      <div className="w-full max-w-md">
-        <ReviewDetailSheet onNext={() => alert('다음 단계로 이동')} />
-      </div>
-    </main>
-  );
+	return (
+		<main className="min-h-screen bg-grey-5 flex items-center justify-center px-4 py-12">
+			<div className="w-full max-w-md">
+				<ReviewDetailSheet onNext={() => alert('다음 단계로 이동')} />
+			</div>
+		</main>
+	);
 }
