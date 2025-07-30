78cedd90d9f99b0f0d2226fdbe5cb3bd4d59ba76
ghkd1418 <1004wipi@gmail.com>
2025-07-30 17:52:56 +0900

    feat: 하나의 리뷰 보기 [TOKTOT-151] - 이미지에 속한 툴팁 표시 - 상태바로 n개의 이미지 중 몇 번째 이미지인지 표시

---
+++ src/features/review/pagenate-images/ui/ImagePaginator.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { Tooltip, TooltipMarker } from '@/entities/review';
+import Image from 'next/image';
+
+import { ProgressBar } from '@/shared/ui/ProgressBar';
+
+import { useImagePagination } from '../lib/useImagePagination';
+import { ReviewImageId, TooltipId } from '@/shared/model/types';
+
+interface ImagePaginatorProps {
+	images: { id: ReviewImageId; url: string; tooltipIds: TooltipId[] }[];
+	tooltips: Record<TooltipId, Tooltip>;
+	onTooltipClick: (tooltip: Tooltip) => void;
+}
+
+export const ImagePaginator = ({
+	images,
+	tooltips,
+	onTooltipClick,
+}: ImagePaginatorProps) => {
+	const { currentIndex, goToNext, goToPrevious } = useImagePagination(
+		images.length,
+	);
+	const currentImage = images[currentIndex];
+
+	const handleTap = (e: React.MouseEvent<HTMLDivElement>) => {
+		if ((e.target as HTMLElement).closest('.tooltip-marker')) {
+			return;
+		}
+
+		const { clientX, currentTarget } = e;
+		const { width } = currentTarget.getBoundingClientRect();
+
+		if (clientX < width / 2) {
+			console.log('go prev');
+			goToPrevious();
+		} else {
+			console.log('go next');
+			goToNext();
+		}
+	};
+
+	return (
+		<div className="w-full h-full cursor-pointer" onClick={handleTap}>
+			<Image
+				src={currentImage.url}
+				alt="리뷰 이미지"
+				layout="fill"
+				objectFit="contain"
+				priority={true}
+			/>
+			{currentImage.tooltipIds.map((id) => {
+				const tooltip = tooltips[id];
+				if (!tooltip) return null;
+
+				return (
+					<div key={id} className="tooltip-marker">
+						<TooltipMarker
+							tip={tooltip}
+							onClick={() => onTooltipClick(tooltip)}
+						/>
+					</div>
+				);
+			})}
+			<div className="absolute bottom-2 left-4 right-4">
+				<ProgressBar total={images.length} current={currentIndex} />
+			</div>
+		</div>
+	);
+};
