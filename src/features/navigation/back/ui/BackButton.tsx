744cd388389bd84f0c59fbc966451766c9b2467a
ghkd1418 <1004wipi@gmail.com>
2025-08-08 13:02:57 +0900

    feat: 뒤로가기 버튼 스타일 변경 가능하도록

--- src/features/navigation/back/ui/BackButton.tsx
+++ src/features/navigation/back/ui/BackButton.tsx
@@ -4,10 +4,14 @@ import { useRouter } from 'next/navigation';
 
 import Icon from '@/shared/ui/Icon';
 
-export const BackButton = () => {
+export const BackButton = ({ className }: { className?: string }) => {
 	const router = useRouter();
 	return (
-		<button onClick={() => router.back()} aria-label="Go back">
+		<button
+			onClick={() => router.back()}
+			aria-label="Go back"
+			className={className}
+		>
 			<Icon name="ArrowLefttBar" />
 		</button>
 	);
