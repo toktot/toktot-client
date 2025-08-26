d51f484ba66bb973ff5bb99ba8d29262153dfbb0
ghkd1418 <1004wipi@gmail.com>
2025-08-26 17:05:14 +0900

    chore: manifast icon 변경 [TOKTOT-450]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,6 @@ export default function Home() {
 	return (
 		<AppShell showBottomNav={true}>
 			<main className="flex-1">
-				<Link href="/login">
-					<button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-						Login
-					</button>
-				</Link>
-				<Link href="/signup">
-					<button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-						Signup
-					</button>
-				</Link>
-				<Link href="/PasswordFind">
-					<button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
-						Find Password
-					</button>
-				</Link>
 				<Link href="/review/write/1">
 					<button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
 						Review
