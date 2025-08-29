10a5c99dd456897eed314bd840bc62e2682cce96
ghkd1418 <1004wipi@gmail.com>
2025-08-29 22:42:07 +0900

    chore: 리뷰쓰기 헤더 여백 수정

--- src/widgets/review/write/ui/ReviewWriteContent.tsx
+++ src/widgets/review/write/ui/ReviewWriteContent.tsx
@@ -28,7 +28,9 @@ export function ReviewWriteContent({
 				<Header.Left>
 					<BackButton />
 				</Header.Left>
-				<Header.Center>리뷰 쓰기</Header.Center>
+				<Header.Center>
+					<span className="leading-[48px]">리뷰 쓰기</span>
+				</Header.Center>
 			</Header>
 			<div className="flex flex-col items-center p-4 gap-9">
 				<VisitedStoreWidget store={visitedStoreData} />
