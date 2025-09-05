d7a97b6a7170279cce2b321974f9c839c66ab6d4
ghkd1418 <1004wipi@gmail.com>
2025-09-05 19:17:56 +0900

    chore: 리뷰 스와이프 애니메이션 개선

--- src/widgets/review/read/ui/ReviewStoryFeed.tsx
+++ src/widgets/review/read/ui/ReviewStoryFeed.tsx
@@ -25,14 +25,12 @@ const swipePower = (offset: number, velocity: number) => {
 const variants = {
 	enter: (direction: number) => ({
 		y: direction > 0 ? '100%' : '-100%',
+		position: 'absolute',
 	}),
-	center: {
-		zIndex: 1,
-		y: 0,
-	},
+	center: { y: 0, position: 'absolute' },
 	exit: (direction: number) => ({
-		zIndex: 0,
 		y: direction < 0 ? '100%' : '-100%',
+		position: 'absolute',
 	}),
 };
 
@@ -60,73 +58,75 @@ export function ReviewStoryFeed() {
 	}
 
 	return (
-		<AnimatePresence initial={false} custom={direction}>
-			<motion.div
-				className="h-full overflow-x-hidden"
-				key={page} // page(currentIndex)가 바뀔 때마다 AnimatePresence가 작동합니다.
-				custom={direction}
-				variants={variants}
-				initial="enter"
-				animate="center"
-				exit="exit"
-				transition={{
-					y: { type: 'spring', stiffness: 300, damping: 30 },
-					opacity: { duration: 0.2 },
-				}}
-				drag="y"
-				dragConstraints={{ top: 0, bottom: 0 }}
-				dragElastic={1}
-				onDragEnd={(e, { offset, velocity }) => {
-					const swipe = swipePower(offset.y, velocity.y);
+		<div className="relative h-full overflow-hidden bg-black">
+			<AnimatePresence initial={false} custom={direction}>
+				<motion.div
+					className="h-full overflow-x-hidden"
+					key={page} // page(currentIndex)가 바뀔 때마다 AnimatePresence가 작동합니다.
+					custom={direction}
+					variants={variants}
+					initial="enter"
+					animate="center"
+					exit="exit"
+					transition={{
+						y: { type: 'spring', stiffness: 300, damping: 30 },
+						opacity: { duration: 0.2 },
+					}}
+					drag="y"
+					dragConstraints={{ top: 0, bottom: 0 }}
+					dragElastic={1}
+					onDragEnd={(e, { offset, velocity }) => {
+						const swipe = swipePower(offset.y, velocity.y);
 
-					if (swipe < -SWIPE_CONFIDENCE_THRESHOLD) {
-						paginate(1);
-					} else if (swipe > SWIPE_CONFIDENCE_THRESHOLD) {
-						paginate(-1);
-					}
+						if (swipe < -SWIPE_CONFIDENCE_THRESHOLD) {
+							paginate(1);
+						} else if (swipe > SWIPE_CONFIDENCE_THRESHOLD) {
+							paginate(-1);
+						}
 
-					if (currentIndex === reviews.length - 2) {
-						fetchNextPage();
-					}
-				}}
-			>
-				<div className="relative flex flex-col h-full">
-					<div className="flex-1 relative">
-						<InteractiveReview reviewId={currentPost.id}>
-							<AnimatePresence>
-								{showGuide && (
-									<InteractionGuide onClose={() => setShowGuide(false)} />
-								)}
-							</AnimatePresence>
-							<ImagePaginator
-								images={currentPost.images}
-								tooltips={currentPost.tooltips}
-								onTooltipClick={handleTooltipClick}
-								selectTooltip={selectedTooltip}
-								onOpenSheet={() => setIsSheetOpen(true)}
-							/>
-						</InteractiveReview>
-					</div>
-					<div className="h-[140px] p-4 text-grey-10 bg-black flex flex-col gap-[14px]">
-						<div className="flex-shrink-0">
-							<ReviewUser author={currentPost.author} />
-						</div>
-
-						<div className="flex gap-2">
-							<div className="bg-grey-90 rounded-xl flex-[4] min-w-0">
-								<ReviewStore storeId={currentPost.store.id} />
-							</div>
-							<div className="bg-grey-90 rounded-xl flex-[1.3] min-w-0">
-								<ReviewStoreWithSheet
-									review={currentPost}
-									isSheetOpen={isSheetOpen}
-									onSheetOpenChange={setIsSheetOpen}
+						if (currentIndex === reviews.length - 2) {
+							fetchNextPage();
+						}
+					}}
+				>
+					<div className="relative flex flex-col h-full">
+						<div className="flex-1 relative">
+							<InteractiveReview reviewId={currentPost.id}>
+								<AnimatePresence>
+									{showGuide && (
+										<InteractionGuide onClose={() => setShowGuide(false)} />
+									)}
+								</AnimatePresence>
+								<ImagePaginator
+									images={currentPost.images}
+									tooltips={currentPost.tooltips}
+									onTooltipClick={handleTooltipClick}
+									selectTooltip={selectedTooltip}
+									onOpenSheet={() => setIsSheetOpen(true)}
 								/>
+							</InteractiveReview>
+						</div>
+						<div className="h-[140px] p-4 text-grey-10 bg-black flex flex-col gap-[14px]">
+							<div className="flex-shrink-0">
+								<ReviewUser author={currentPost.author} />
+							</div>
+
+							<div className="flex gap-2">
+								<div className="bg-grey-90 rounded-xl flex-[4] min-w-0">
+									<ReviewStore storeId={currentPost.store.id} />
+								</div>
+								<div className="bg-grey-90 rounded-xl flex-[1.3] min-w-0">
+									<ReviewStoreWithSheet
+										review={currentPost}
+										isSheetOpen={isSheetOpen}
+										onSheetOpenChange={setIsSheetOpen}
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</motion.div>
-		</AnimatePresence>
+				</motion.div>
+			</AnimatePresence>
+		</div>
 	);
 }
