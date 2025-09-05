5540e2ae7d26a11de222815f324beb6257a3d82e
hyewon <won10201@kookmin.ac.kr>
2025-09-06 07:21:23 +0900

    feat: 수정

--- src/app/goodstore/page.tsx
+++ src/app/goodstore/page.tsx
@@ -1,6 +1,11 @@
 // goodstore/page.tsx
 // 여기는 최소한만
+'use client';
+
 import GoodstoreClient from '@/features/home/components/GoodStoreClient';
+
+// goodstore/page.tsx
+// 여기는 최소한만
 
 export default function Page() {
 	return <GoodstoreClient />;
