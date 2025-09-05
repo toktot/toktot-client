f5a1694d2ac16a9bc079a152f6e8f92fc0ab7eb1
ghkd1418 <1004wipi@gmail.com>
2025-09-05 16:02:01 +0900

    chore: 스크롤바 제거

--- src/widgets/review/read/ui/ReviewTooltipSheet.tsx
+++ src/widgets/review/read/ui/ReviewTooltipSheet.tsx
@@ -25,7 +25,7 @@ export const ReviewTooltipSheet = ({ open, onOpenChange, review }: Props) => {
 	return (
 		<BottomSheet open={open} onOpenChange={onOpenChange}>
 			<BottomSheetOverlay className="fixed inset-0 z-40 bg-black/60" />
-			<BottomSheetContent className="fixed bottom-0 z-50 w-full max-h-[750px] rounded-t-2xl bg-white shadow-lg px-2 flex flex-col">
+			<BottomSheetContent className="fixed bottom-0 z-50 w-full max-h-[80vh] rounded-t-2xl bg-white shadow-lg px-2 flex flex-col">
 				<div className="mx-auto my-3 h-1 w-6 rounded-full bg-grey-30" />
 
 				<div className="flex-shrink-0">
